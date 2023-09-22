fead440bd281080162d5daa1d82e7c4bcacabb79
Bosco Lam <121968040+bosco1230@users.noreply.github.com>
2023-09-21 21:54:31 -0700

    showing total items in cart

--- assets/scripts/ShoppingCart.js
+++ assets/scripts/ShoppingCart.js
@@ -1,20 +1,21 @@
 export class ShoppingCart {
     constructor() {
       this._items = [];
+      this.cartCount = 0; // Initialize the cartCount property
     }
   
     addProduct(product) {
       this._items.push(product);
       this.updateTotal();
     }
+  
     removeProduct(product) {
-        const index = this._items.indexOf(product);
-        if (index !== -1) {
-          this._items.splice(index, 1);
-          this.updateTotal();
-        }
+      const index = this._items.indexOf(product);
+      if (index !== -1) {
+        this._items.splice(index, 1);
+        this.updateTotal();
       }
-    
+    }
   
     set items(updatedItems) {
       this._items = [...updatedItems];
@@ -28,15 +29,23 @@ export class ShoppingCart {
     }
   
     updateTotal() {
-      const spanTotal = document.getElementById("totalCart");
-      spanTotal.textContent = `$${this.total}`;
-    }
-  
-    render() {
+        const spanTotal = document.getElementById("totalCart");
+        spanTotal.textContent = `$${this.total}`;
+      
+        // Update the cartCount property based on the number of items in the cart
+        this.cartCount = this._items.length;
+      
+        // Update the cart count display with "items in cart"
+        const cartCountBadge = document.getElementById("cartCountBadge");
+        cartCountBadge.textContent = `${this.cartCount} item${this.cartCount !== 1 ? 's' : ''} in cart`;
+      }
+      
+      render() {
         const section = document.createElement("section");
         section.classList.add("cart");
         section.innerHTML = `<h2>Total: <span id="totalCart">$${this.total}</span></h2>
-            <button>Order Now!</button>`;
+          <button class="order-now-button">Order Now! <span id="cartCountBadge" class="badge">${this.cartCount} item${this.cartCount !== 1 ? 's' : ''} in cart</span></button>`;
         return section;
-    }
+      }
+      
   }
