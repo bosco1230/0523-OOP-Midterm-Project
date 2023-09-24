b00ac6c45b70f05f0c1144974da720a2e534ebc2
Bosco Lam <121968040+bosco1230@users.noreply.github.com>
2023-09-24 12:48:01 -0700

    done project

--- assets/scripts/ProductItem.js
+++ assets/scripts/ProductItem.js
@@ -1,56 +1,122 @@
-// ProductItem.js
 import { App } from "./app.js";
 import { Product } from "./Product.js";
 
 export class ProductItem extends Product {
   constructor(product) {
     super(product);
-    this.quantity = 0; // Initialize quantity to 0
+    this.quantity = 0;
   }
 
   addToCart() {
-    this.quantity++; // Increment the quantity
+    this.quantity++; 
     App.addProductToCart(this);
+    this.updateMainQuantity(); 
   }
 
   removeFromCart() {
     if (this.quantity > 0) {
-      this.quantity--; // Decrement the quantity, but prevent it from going negative
-      App.removeProductFromCart(this);
+      this.quantity--; 
+        App.removeProductFromCart(this);
+      this.updateMainQuantity(); 
+    }
+  }
+
+  showProductPopup(productId) {
+    const popupContainer = document.createElement("div");
+    popupContainer.classList.add("product-popup");
+
+    popupContainer.innerHTML = `
+      <div class="popup-content">
+        <div>
+          <img src="${this.img}" alt="${this.title}" />
+        </div>
+        <div class="product-item__popup">
+          <div class="title-container">
+            <h2 class="popup-title">${this.title}</h2>
+          </div>
+          <h3>$${this.price}</h3>
+          <div class="description-container-popup">
+            <p class="description">${this.description}</p>
+          </div>
+          <button class="remove-btn">-</button>
+          <span class="quantity">${this.quantity}</span>
+          <button class="add-btn">+</button>
+          <button class="close-popup">Close</button>
+        </div>
+      </div>
+    `;
+
+    const closePopupBtn = popupContainer.querySelector(".close-popup");
+    closePopupBtn.addEventListener("click", () => {
+      document.body.removeChild(popupContainer);
+    });
+
+    const addPopupBtn = popupContainer.querySelector(".add-btn");
+    addPopupBtn.addEventListener("click", () => {
+      this.addToCart();
+      const quantitySpan = popupContainer.querySelector(".quantity");
+      quantitySpan.textContent = `${this.quantity}`;
+      this.updateMainQuantity(); 
+    });
+
+    const removePopupBtn = popupContainer.querySelector(".remove-btn");
+    removePopupBtn.addEventListener("click", () => {
+      this.removeFromCart();
+      const quantitySpan = popupContainer.querySelector(".quantity");
+      quantitySpan.textContent = `${this.quantity}`;
+      this.updateMainQuantity(); 
+    });
+
+    document.body.appendChild(popupContainer);
+  }
+
+  updateMainQuantity() {
+    const mainProductLi = document.querySelector(`.product-item[data-product-id="${this.id}"]`);
+    if (mainProductLi) {
+      const mainQuantitySpan = mainProductLi.querySelector(".quantity");
+      mainQuantitySpan.textContent = `${this.quantity}`;
     }
   }
 
   render() {
     const li = document.createElement("li");
     li.classList.add("product-item");
+    li.setAttribute("data-product-id", this.id); 
+
     li.innerHTML = `<div>
-      <img
-        src="${this.img}"
-        alt="${this.title}"
-      />
+      <img src="${this.img}" alt="${this.title}" />
       <div class="product-item__content">
-        <h2>${this.title}</h2>
+        <div class="title-container">
+          <h2 class="title">${this.title}</h2>
+        </div>
         <h3>$${this.price}</h3>
-        <p>${this.description}</p>
+        <div class="description-container">
+          <p class="description">${this.description}</p>
+        </div>
         <button class="remove-btn">-</button>
         <span class="quantity">${this.quantity}</span>
         <button class="add-btn">+</button>
+        <button class="show-popup">More Details</button>
       </div>
     </div>`;
+
     const addBtn = li.querySelector(".add-btn");
     const removeBtn = li.querySelector(".remove-btn");
-    const quantitySpan = li.querySelector(".quantity"); // Get the quantity span
-  
+    const showPopupBtn = li.querySelector(".show-popup");
+
+    showPopupBtn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      this.showProductPopup(this.id);
+    });
+
     addBtn.addEventListener("click", () => {
       this.addToCart();
-      quantitySpan.textContent = `${this.quantity}`; // Update the quantity span when adding
     });
-  
+
     removeBtn.addEventListener("click", () => {
       this.removeFromCart();
-      quantitySpan.textContent = `${this.quantity}`; // Update the quantity span when removing
     });
-  
+
     return li;
   }
 }
--- assets/scripts/ShoppingCart.js
+++ assets/scripts/ShoppingCart.js
@@ -1,14 +1,17 @@
 export class ShoppingCart {
     constructor() {
       this._items = [];
-      this.cartCount = 0; // Initialize the cartCount property
+      this.cartCount = 0; 
     }
   
     addProduct(product) {
       this._items.push(product);
       this.updateTotal();
     }
-  
+    clearCart() {
+      this._items = [];
+      this.updateTotal();
+    }
     removeProduct(product) {
       const index = this._items.indexOf(product);
       if (index !== -1) {
@@ -32,10 +35,8 @@ export class ShoppingCart {
         const spanTotal = document.getElementById("totalCart");
         spanTotal.textContent = `$${this.total}`;
       
-        // Update the cartCount property based on the number of items in the cart
         this.cartCount = this._items.length;
       
-        // Update the cart count display with "items in cart"
         const cartCountBadge = document.getElementById("cartCountBadge");
         cartCountBadge.textContent = `${this.cartCount} item${this.cartCount !== 1 ? 's' : ''} in cart`;
       }
@@ -43,9 +44,95 @@ export class ShoppingCart {
       render() {
         const section = document.createElement("section");
         section.classList.add("cart");
-        section.innerHTML = `<h2>Total: <span id="totalCart">$${this.total}</span></h2>
-          <button class="order-now-button">Order Now! <span id="cartCountBadge" class="badge">${this.cartCount} item${this.cartCount !== 1 ? 's' : ''} in cart</span></button>`;
+        section.innerHTML = `
+          <h2>Total: <span id="totalCart">$${this.total}</span></h2>
+          <button class="order-now-button" id="orderNowBtn">Order Now! 
+            <span id="cartCountBadge" class="badge">${this.cartCount} item${this.cartCount !== 1 ? 's' : ''} in cart</span>
+          </button>
+          <div id="myModal" class="modal">
+            <div class="modal-content">
+              <span class="close" id="closeModal">&times;</span>
+              <h3>Order Form</h3>
+              <form id="myForm">
+              <input type="text" placeholder="Name" required>
+              <input type="email" placeholder="Email" required>
+              <input type="tel" placeholder="Phone Number" required>
+              <input type="text" placeholder="Address" required>
+              <input type="text" placeholder="Postal Code" required>              
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+          </div>
+          <div id="errorModal" class="modal">
+            <div class="modal-content">
+              <span class="close" id="closeErrorModal">&times;</span>
+              <h3>Error</h3>
+              <p>Your cart is empty. Add items to the cart before placing an order.</p>
+            </div>
+          </div>
+          <div id="successModal" class="modal">
+          <div class="modal-content">
+            <span class="close" id="closeSuccessModal">&times;</span>
+            <h3>Order Submitted</h3>
+            <p>You'll receive the package in ${getRandomNumber(7, 13)}-${getRandomNumber(14, 30)} days.</p>
+          </div>
+        </div>
+        `;
+    
+        const orderNowBtn = section.querySelector("#orderNowBtn");
+        const modal = section.querySelector("#myModal");
+        const closeModal = section.querySelector("#closeModal");
+        const errorModal = section.querySelector("#errorModal");
+        const closeErrorModal = section.querySelector("#closeErrorModal");
+        const successModal = section.querySelector("#successModal");
+        const closeSuccessModal = section.querySelector("#closeSuccessModal");
+        const myForm = section.querySelector("#myForm");
+   
+        myForm.addEventListener("submit", (event) => {
+          event.preventDefault(); 
+    
+          if (this.cartCount === 0) {
+            errorModal.style.display = "block";
+          } else {
+            this.clearCart(); 
+            successModal.style.display = "block";
+            modal.style.display = "none"; 
+          }
+        });
+    
+    
+        closeSuccessModal.addEventListener("click", () => {
+          successModal.style.display = "none"; 
+        });
+        orderNowBtn.addEventListener("click", () => {
+          if (this.cartCount === 0) {
+            errorModal.style.display = "block"; 
+          } else {
+            modal.style.display = "block"; 
+          }
+        });
+    
+        closeModal.addEventListener("click", () => {
+          modal.style.display = "none"; 
+        });
+    
+        closeErrorModal.addEventListener("click", () => {
+          errorModal.style.display = "none"; 
+        });
+    
+        window.addEventListener("click", (event) => {
+          if (event.target === modal) {
+            modal.style.display = "none"; 
+          }
+          if (event.target === errorModal) {
+            errorModal.style.display = "none";
+          }
+        });
+    
         return section;
       }
       
+  }
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
